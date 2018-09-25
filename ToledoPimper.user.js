c0daf0d9624956349c2b610a96212dad30bc32e3
Sleeyax <yourd3veloper@gmail.com>
2018-09-25 22:31:31 +0200

    Restructure project, fix bug

--- ToledoPimper.user.js
+++ ToledoPimper.user.js
@@ -4,13 +4,13 @@
 // @description	Make toledo less boring
 // @author		Sleeyax
 // @include		*toledo.kuleuven.be/portal*
-// @version		1.0
+// @version		1.2
 // @require		http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
-// @require		lib/jBox.js
+// @require		inc/jBox.js
 // @require	    Toledo.js
-// @require	    CoursePimper.js
-// @resource	jBoxCSS lib/jBox.css
-// @resource	form form.html
+// @require	    lib/CoursePimper/CoursePimper.js
+// @resource	jBoxCSS inc/jBox.css
+// @resource	form lib/CoursePimper/form.html
 // @grant		GM_addStyle
 // @grant		GM_getResourceText
 // @grant		GM_setValue
@@ -23,8 +23,8 @@ let toledo = new Toledo();
 
 toledo.onCoursesLoaded(() => {
     let pimper = new CoursePimper();
-    pimper.replaceCourseCoverImages();
+    pimper.replaceCoverImages();
     toledo.onCourseSettingsLoaded(() => {
-        pimper.enablePimpSettings();
+        pimper.insertControl();
     });
 });
